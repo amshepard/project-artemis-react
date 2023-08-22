113823ad6c411eae8b14a733e1b9edafec5a59d7
Antoine M. Shepard <antoineshepard@pursuit.org>
2023-08-21 20:29:55 -0400

    Styling changes.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './arty.gif';
+import logo from './artemis.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Logo-container">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="Paragraph">
+          Inspired by the fierce and protective spirit of the Greek goddess Artemis, The Artemis Project embodies the essence of this mighty huntress in a modern context. Just as Artemis was known for her unparalleled ability to track and find elusive prey, our app channels her unwavering dedication into helping you locate your lost companion. <br></br><br></br>Much like Artemis' watchful gaze over the wilderness, The Artemis Project casts its digital net far and wide, harnessing the power of modern technology to connect those in search of their furry friends. With every search, every share, and every heartwarming reunion, we honor Artemis' legacy by bringing her guiding light to the modern age, reimagined as a beacon of hope for those seeking to reunite with their beloved pets.
+          </p>
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Custom-link"
+          href="http://localhost:3001/pets" 
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter The Artemis Project Here
         </a>
       </header>
     </div>
