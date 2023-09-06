9db40b6882c90d259993004296ab2b73d10eee45
Antoine M. Shepard <antoineshepard@pursuit.org>
2023-09-05 21:03:38 -0400

    .env update to route to proper port.

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -2,6 +2,7 @@ import Pets from "../Components/Pets";
 import { NavLink } from "react-router-dom";
 
 function Index() {
+  console.log("Loading the index page.")
   return (
     <>
       <div className="Index">
